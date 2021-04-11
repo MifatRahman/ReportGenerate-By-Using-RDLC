bf0ff726fdcecb24cb58d04542bfeef7e3dd3585
mifat <mifat.swe@gmail.com>
2021-04-11 22:30:50 +0600

    Add project files.

---
+++ ReportGenerate/Controllers/ReportController.cs
@@ -0,0 +1,37 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReportGenerate.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ReportController(IWebHostEnvironment webHostEnvironment) 
+        {
+            this._webHostEnvironment = webHostEnvironment;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Print() 
+        {
+            string mimtype = "";
+            int extention = 1;
+            var path = $"{this._webHostEnvironment.WebRootPath}\\Reports\\Report1.rdlc";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("rp1","Welcome to Online Class");
+            LocalReport localReport = new LocalReport(path);
+            var result = localReport.Execute(RenderType.Pdf,extention, parameters, mimtype);
+            return File(result.MainStream, "application/pdf");
+        }
+    }
+}
